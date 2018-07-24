17083cb5ca5fe84bf027feb461bbb9827ddc28e9
arthurtruong <arthurtruong.work@gmail.com>
2018-07-24 13:18:53 +1000

    fix mailgun

---
+++ db/migrate/20180724024931_remove.rb
@@ -0,0 +1,4 @@
+class Remove < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_22_084339) do
+ActiveRecord::Schema.define(version: 2018_07_24_024931) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
